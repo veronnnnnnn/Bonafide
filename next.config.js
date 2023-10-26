514b8f86881b4e17ba9ed2f0ad4529a02e5bbf46
Veronica Silva <veronas.0935@gmail.com>
2023-10-26 06:50:34 +0100

    security

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {async headers() {
+    return [
+      {
+        // matching all routes
+        source: '/(.*)',
+        headers: [
+          { key: 'X-Frame-Options', value: 'DENY' },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'Referrer-Policy', value: 'no-referrer-when-downgrade' },
+          { key: 'Content-Security-Policy', value: "default-src 'self'" },
+          { key: 'Permissions-Policy',  value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()'}
+        ],
+      },
+    ]
+  }, 
+}
+
 
 module.exports = nextConfig
