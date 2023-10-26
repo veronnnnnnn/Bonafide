235844b0b76ddb1e4463dd4f5a1bf9da7a9ac295
Veronica Silva <veronas.0935@gmail.com>
2023-10-26 06:56:37 +0100

    update security

--- next.config.js
+++ next.config.js
@@ -1,20 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {async headers() {
-    return [
-      {
-        // matching all routes
-        source: '/(.*)',
-        headers: [
-          { key: 'X-Frame-Options', value: 'DENY' },
-          { key: 'X-Content-Type-Options', value: 'nosniff' },
-          { key: 'Referrer-Policy', value: 'no-referrer-when-downgrade' },
-          { key: 'Content-Security-Policy', value: "default-src 'self'" },
-          { key: 'Permissions-Policy',  value: 'camera=(), microphone=(), geolocation=(), browsing-topics=()'}
-        ],
-      },
-    ]
-  }, 
-}
-
-
-module.exports = nextConfig
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          // matching all routes
+          source: '/(.*)',
+          headers: [
+            { key: 'X-Frame-Options', value: 'DENY' },
+            { key: 'X-Content-Type-Options', value: 'nosniff' },
+            { key: 'Referrer-Policy', value: 'no-referrer-when-downgrade' },
+            { key: 'Content-Security-Policy', value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self';" },
+            { key: 'Permissions-Policy',  value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()' }
+          ],
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
+  
