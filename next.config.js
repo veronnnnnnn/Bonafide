d2b1e0d90ae775439edab0aa505599bc6e8458e4
Veronica Silva <veronas.0935@gmail.com>
2023-10-26 07:08:14 +0100

    updatecsp

--- next.config.js
+++ next.config.js
@@ -9,8 +9,8 @@ const nextConfig = {
             { key: 'X-Frame-Options', value: 'DENY' },
             { key: 'X-Content-Type-Options', value: 'nosniff' },
             { key: 'Referrer-Policy', value: 'no-referrer-when-downgrade' },
-            { key: 'Content-Security-Policy', value: "default-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https://www.google.com; connect-src 'self' https://firestore.googleapis.com;" },
-            { key: 'Permissions-Policy',  value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()' }
+            { key: 'Content-Security-Policy', value: "default-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https://www.google.com; connect-src 'self' https://firestore.googleapis.com; font-src 'self' https://fonts.gstatic.com; script-src 'self' 'unsafe-inline';" },
+            { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()' }
           ],
         },
       ];
@@ -18,5 +18,6 @@ const nextConfig = {
   };
   
   module.exports = nextConfig;
+  
   
   
