ea7be271cfb52fe0c37d940e59c5d9cac0244502
Veronica Silva <veronas.0935@gmail.com>
2023-10-26 07:02:15 +0100

    new csp

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
             { key: 'X-Frame-Options', value: 'DENY' },
             { key: 'X-Content-Type-Options', value: 'nosniff' },
             { key: 'Referrer-Policy', value: 'no-referrer-when-downgrade' },
-            { key: 'Content-Security-Policy', value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self';" },
+            { key: 'Content-Security-Policy', value: "default-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https://www.google.com; connect-src 'self' https://firestore.googleapis.com;" },
             { key: 'Permissions-Policy',  value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()' }
           ],
         },
@@ -18,4 +18,5 @@ const nextConfig = {
   };
   
   module.exports = nextConfig;
+  
   
