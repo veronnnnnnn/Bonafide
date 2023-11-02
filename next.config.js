9c084ec4f7801862015ae8169ee73b470da204ba
Veronica Silva <veronas.0935@gmail.com>
2023-11-02 13:18:12 +0000

    update sec

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
           { key: 'Content-Security-Policy', value: "default-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https://www.google.com; connect-src 'self' https://firestore.googleapis.com; font-src 'self' https://fonts.gstatic.com; script-src 'self' 'unsafe-inline';" },
           { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()' },
           { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains; preload' },
-          { key: 'Expect-CT', value: 'enforce, max-age=604800' }
+          { key: 'Expect-CT', value: "enforce, max-age=30, report-uri='https://example.report-uri.com/r/d/ct/enforce'" }
         ],
       },
     ];
