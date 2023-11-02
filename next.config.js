0bc8b89c753b4f5799da302ceb80d05f283a41ed
Veronica Silva <veronas.0935@gmail.com>
2023-11-02 13:05:14 +0000

    update securityy

--- next.config.js
+++ next.config.js
@@ -1,23 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-      return [
-        {
-          // matching all routes
-          source: '/(.*)',
-          headers: [
-            { key: 'X-Frame-Options', value: 'DENY' },
-            { key: 'X-Content-Type-Options', value: 'nosniff' },
-            { key: 'Referrer-Policy', value: 'no-referrer-when-downgrade' },
-            { key: 'Content-Security-Policy', value: "default-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https://www.google.com; connect-src 'self' https://firestore.googleapis.com; font-src 'self' https://fonts.gstatic.com; script-src 'self' 'unsafe-inline';" },
-            { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()' }
-          ],
-        },
-      ];
-    },
-  };
-  
-  module.exports = nextConfig;
-  
-  
-  
+  async headers() {
+    return [
+      {
+        // matching all routes
+        source: '/(.*)',
+        headers: [
+          { key: 'X-Frame-Options', value: 'DENY' },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'Referrer-Policy', value: 'no-referrer-when-downgrade' },
+          { key: 'Content-Security-Policy', value: "default-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' data: https://www.google.com; connect-src 'self' https://firestore.googleapis.com; font-src 'self' https://fonts.gstatic.com; script-src 'self' 'unsafe-inline';" },
+          { key: 'Permissions-Policy', value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()' },
+          { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains; preload' },
+          { key: 'Expect-CT', value: 'enforce, max-age=604800' }
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
